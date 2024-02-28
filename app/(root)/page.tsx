e220dd5e92326ffc71c3f49a1a789dbcc4b9bb0c
matheusrodrigues1 <matech00@outlook.com>
2024-02-28 14:26:12 -0300

    Layout Sidebar & Mobile Navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,13 +2,10 @@ import { UserButton } from "@clerk/nextjs"
 
 const Home = async () => {
   return (
-    <>
       <div>
         <p>Home</p>
-        <UserButton afterSignOutUrl="/"/>
       </div>
-    </>
   )
-}
+};
 
-export default Home
+export default Home;
